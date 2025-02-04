a97bf68f3a5597d0e36cadb14956622636930cb0
UTheCat <bees9633@gmail.com>
2025-02-03 21:05:12 -0800

    Create arch_gaming_vm_setup.sh

---
+++ arch_gaming_vm_setup.sh
@@ -0,0 +1,70 @@
+# Setup script for Arch Linux Gaming VM
+# This script assumes it's being run on a VMWare virtual machine.
+
+# Update system
+echo "Updating system..."
+sudo pacman -Syu
+
+# Gnome Desktop Manager
+echo "Installing Gnome Desktop Manager..."
+sudo pacman -Sy gdm
+echo "Enabling gdm"
+sudo systemctl enable gdm.service
+
+# Gamescope
+echo "Installing gamescope..."
+sudo pacman -Sy gamescope
+
+# Git
+echo "Installing Git..."
+sudo pacman -Sy git
+
+# VSCode
+echo "Installing VSCode..."
+sudo pacman -Sy code
+
+# .NET
+echo "Installing .NET..."
+sudo pacman -Sy dotnet-sdk
+
+# Firefox
+echo "Installing Firefox..."
+sudo pacman -Sy firefox
+
+# Godot (Mono)
+echo "Installing godot-mono..."
+sudo pacman -Sy godot-mono
+
+# Flatpak
+echo "Installing Flatpak..."
+sudo pacman -Sy flatpak
+
+# open-vm-tools (Open VMWare Tools)
+echo "Installing open-vm-tools..."
+sudo pacman -Sy open-vm-tools
+echo "Enabling open-vm-tools"
+sudo systemctl enable open-vm-tools
+
+# ProtonVPN
+echo "Installing ProtonVPN..."
+sudo pacman -Sy proton-vpn-gtk-app
+
+# Sober (Runs Roblox on Linux)
+echo "Installing Sober..."
+flatpak install -u org.vinegarhq.Sober
+
+# User directories
+echo "Making ~/bin directory..."
+mkdir ~/bin
+
+echo "Making ~/repos directory..."
+mkdir ~/repos
+
+# Git config
+echo "Configuring Git..."
+git config --global user.name "Anonymous"
+git config --global user.email "<>"
+
+# Done.
+echo "Done. Now rebooting..."
+reboot
